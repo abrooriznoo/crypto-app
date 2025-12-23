4d145f63c7c505a69471fd22d23f9652b569bc80
Abroor Rizky <abroorrizky@gmail.com>
2025-12-23 11:02:01 +0700

    Fix Error on Vercel v4

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), legacy()],
-  base: "/home",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
